a8fdcfa2bab72acb1401a98ed7913458a4cd3796
tonyn9 <tonyn9@uci.edu>
2017-03-13 18:48:34 -0700

    Create mm.sh

---
+++ mm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#$ -N MM
+#$ -q eecs117
+
+# Runs a bunch of standard command-line
+# utilities, just as an example:
+
+echo "Script began:" `date`
+echo "Node:" `hostname`
+echo "Current directory: ${PWD}"
+
+echo ""
+echo "=== Running 5 trials of Matrix Multiple with 1024 1024 1024 ettings ... ==="
+for trial in 1 2 3 4 5 ; do
+  echo "*** Trial ${trial} ***"
+  ./mm 1024 1024 1024
+done
+
+echo ""
+echo "=== Done! ==="
+
+# eof
